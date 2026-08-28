e020673a1c84667b61fea9d56f3d91d242b153db
Daniel Thompson <daniel@redfelineninja.org.uk>
2026-08-28 20:08:27 +0100

    sparks: Add a --history option

--- src/sparks.rs
+++ src/sparks.rs
@@ -3,7 +3,10 @@
 
 use clap::Args;
 use nucs::{table, Result};
-use std::io::{self, Write};
+use std::{
+    collections::VecDeque,
+    io::{self, Write},
+};
 
 /// Show a table as a sparkline
 #[derive(Args, Debug)]
@@ -11,41 +14,83 @@ pub struct Command {
     /// Watch the live results (animate a single line)
     #[arg(short, long)]
     watch: bool,
+
+    /// Show history during live updates
+    #[arg(short = 'H', long)]
+    history: bool,
 }
 
-fn sparkline(data: &[f64], max: f64) -> String {
-    let mut s = String::new();
-    for v in data.iter() {
-        s.push(if *v < (0.125 * max) {
-            '▁'
-        } else if *v < (0.25 * max) {
-            '▂'
-        } else if *v < (0.375 * max) {
-            '▃'
-        } else if *v < (0.5 * max) {
-            '▄'
-        } else if *v < (0.625 * max) {
-            '▅'
-        } else if *v < (0.75 * max) {
-            '▆'
-        } else if *v < (0.875 * max) {
-            '▇'
-        } else {
-            '█'
-        });
+fn spark(val: f64, max: f64) -> char {
+    if val < (0.125 * max) {
+        '▁'
+    } else if val < (0.25 * max) {
+        '▂'
+    } else if val < (0.375 * max) {
+        '▃'
+    } else if val < (0.5 * max) {
+        '▄'
+    } else if val < (0.625 * max) {
+        '▅'
+    } else if val < (0.75 * max) {
+        '▆'
+    } else if val < (0.875 * max) {
+        '▇'
+    } else {
+        '█'
     }
+}
+
+fn sparkline<'a>(data: impl IntoIterator<Item = &'a f64>, max: f64) -> String {
+    let mut s = String::new();
+
+    for v in data {
+        s.push(spark(*v, max));
+    }
+
     s
 }
 
 pub fn app(args: &Command) -> Result<()> {
+    let columns: usize = crossterm::terminal::size()
+        .map(|(w, _)| w as usize)
+        .unwrap_or(80);
+
     let (_headings, rows) = table::rows(io::stdin().lock())?;
+
+    let mut history = VecDeque::new();
+
     for row in rows {
         if let Some(vals) = row.as_percent() {
-            if args.watch {
-                print!("    {}\r", sparkline(&vals, 100.0));
+            let sparks = sparkline(&vals, 100.0);
+
+            if args.history {
+                let mut avg = vals.iter().sum::<f64>() / vals.len() as f64;
+
+                // this is numerically "wrong" but ensures that big single
+                // values appear as a tiny spike in the history line
+                if vals.iter().copied().max_by(f64::total_cmp).unwrap_or(0.0) > 66.67 {
+                    avg = avg.max(0.125);
+                }
+                history.push_front(avg);
+
+                // adjust history to fit within terminal width
+                let len = columns.saturating_sub(12 + vals.len());
+                if history.len() < len {
+                    history.resize(len, 0.0);
+                }
+                while history.len() > len {
+                    history.pop_back();
+                }
+
+                let history = sparkline(&history, 100.0);
+
+                print!("    {sparks}    {history}\x1b[K\r");
+                io::stdout().flush()?;
+            } else if args.watch {
+                print!("    {sparks}\x1b[K\r");
                 io::stdout().flush()?;
             } else {
-                println!("{}", sparkline(&vals, 100.0));
+                println!("{sparks}");
             }
         }
     }
